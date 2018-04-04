fcd4da9434836a03d0b4b9d24d094bf99e66a83c
dimayasin <dyasin@gmail.com>
2018-04-04 11:46:51 -0500

    Loading Page

---
+++ MEAN/JAVASCRIPT/JS OOP/DeckOfCards.js
@@ -0,0 +1,13 @@
+class Deck{
+    static Suits=["Clubs", "Spades", "Hearts", "Diamonds"];
+    static Values=[ "Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King" ];
+    constructor(){
+        this.deck=[];
+        for (var s of Suits) {   //(var i=0; i<Suit.length;i++){
+            for( var v of Values ){
+                
+            }
+        }
+
+    }
+}
--- MEAN/JAVASCRIPT/JS OOP/NinjaClassIII.js
+++ MEAN/JAVASCRIPT/JS OOP/NinjaClassIII.js
@@ -9,7 +9,7 @@ class Ninja{
         console.log("My ninja name is: ",this.name);
     }
     showStats(){
-        console.log("Name: "+this.name+ ", Health: "+this.health+ ", Speed: "+this.speed+", Strength: "+this.strength);
+        console.log("Name: "+this.name+ ", Health: " + this.health + ", Speed: "+this.speed+", Strength: "+this.strength);
     }
     drinkSake(){
         this.health +=10;
